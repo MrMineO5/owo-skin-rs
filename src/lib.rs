0a2a347fd9579cab465e95f7ead3b00cb7f30def
MrMineO5 <petzmagnus@gmail.com>
2025-04-11 21:38:03 +0200

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,30 @@
+mod network;
+
+pub fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use std::net::UdpSocket;
+    use std::thread::sleep;
+    use std::time::Duration;
+    use crate::network::UDPNetwork;
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let network = UDPNetwork::new();
+
+        loop {
+            if let Some((data, src)) = network.recv_from() {
+                println!("Received: {} from {}", data, src);
+            } else {
+                network.send("ping")
+            }
+
+            sleep(Duration::from_secs(3));
+        }
+
+    }
+}
---
+++ src/network.rs
@@ -0,0 +1,26 @@
+use std::net::{SocketAddr, UdpSocket};
+
+pub struct UDPNetwork {
+    socket: UdpSocket
+}
+impl UDPNetwork {
+    pub fn new() -> UDPNetwork {
+        let socket = UdpSocket::bind("127.0.0.1:0").unwrap();
+        socket.set_broadcast(true).unwrap();
+
+        UDPNetwork { socket }
+    }
+
+    pub fn recv_from(&self) -> Option<(&str, SocketAddr)> {
+        let mut buf = [0; 1024];
+        if let Ok((amt, src)) = self.socket.recv_from(&mut buf) {
+            Option::Some((String::from_utf8_lossy(&buf[..amt]).as_ref(), src))
+        } else {
+            Option::None
+        }
+    }
+
+    pub fn send(&self, data: &str) {
+        self.socket.send(data.as_bytes()).unwrap();
+    }
+}
