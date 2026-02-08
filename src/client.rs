9ed43dadf29a2bf87b53c0e88b5283b23d61d60a
UltraDev <petzmagnus@gmail.com>
2026-02-08 09:40:53 +0100

    Add non-blocking connect functions

--- src/client.rs
+++ src/client.rs
@@ -5,6 +5,9 @@ use std::thread::sleep;
 use std::time::Duration;
 use crate::auth::GameAuth;
 
+const OWO_PORT: u16 = 54020;
+const AUTO_CONNECT_ADDRESS: (&str, u16) = ("255.255.255.255", OWO_PORT);
+
 pub struct Client {
     auth: GameAuth,
     network: UDPNetwork,
@@ -16,25 +19,35 @@ impl Client {
     }
 
     pub fn auto_connect(&self) {
-        self.connect(("255.255.255.255", 54020))
+        self.connect(AUTO_CONNECT_ADDRESS)
     }
 
     pub fn connect<A: ToSocketAddrs>(&self, addr: A) {
         loop {
-            if let Some((data, src)) = self.network.recv_from() {
-                if data == "okay" {
-                    let message = format!("{}*AUTH*{}", self.auth.id, self.auth.baked_sensations);
-                    self.network.send_to(message.as_str(), src);
-                } else if data == "pong" {
-                    self.network.connect(src);
-                    break;
-                }
-            } else {
-                self.network.send_to("ping", &addr);
+            if self.connect_non_blocking(&addr) {
+                break;
             }
-
-            sleep(Duration::from_secs(3));
         }
+    }
+
+    pub fn auto_connect_non_blocking(&self) -> bool {
+        self.connect_non_blocking(&AUTO_CONNECT_ADDRESS)
+    }
+    pub fn connect_non_blocking<A: ToSocketAddrs>(&self, addr: &A) -> bool {
+        self.network.send_to("ping", &addr);
+
+        sleep(Duration::from_secs(3));
+
+        if let Some((data, src)) = self.network.recv_from() {
+            if data == "okay" {
+                let message = format!("{}*AUTH*{}", self.auth.id, self.auth.baked_sensations);
+                self.network.send_to(message.as_str(), src);
+            } else if data == "pong" {
+                self.network.connect(src);
+                return true;
+            }
+        }
+        false
     }
 
     pub fn send_sensation(&self, sensation: Sensation) {
